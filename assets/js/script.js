841154ebb55c03b758af66ebe4041f831f8a1505
DSeV0 <m.menares.b01@gmail.com>
2026-06-16 18:34:40 -0400

    Comienzo de Portafolio Real, página de inicio/home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
 console.log("hola carlita");
 
-// Obtener el botón y agregar evento de clic
-const duplicarBtn = document.getElementById('duplicarBtn');
-const textoDoble = document.querySelector('.doble');
 
-duplicarBtn.addEventListener('click', function() {
-    // Crear un nuevo párrafo con el mismo contenido y estilos
-    const nuevoParrafo = document.createElement('p');
-    nuevoParrafo.className = 'doble'; // Mantener la misma clase para los estilos
-    nuevoParrafo.textContent = textoDoble.textContent;
-    
-    // Agregar el nuevo párrafo después del existente
-    textoDoble.parentNode.insertBefore(nuevoParrafo, textoDoble.nextSibling);
-});
+
